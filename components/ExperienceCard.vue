0698184cfea03044ca85a6ec2a728f96e2d36757
Kha Nguyen <nguyencaokha131995@gmail.com>
2023-10-18 11:45:13 +0200

    [lighthouse] fix aria-label

--- components/ExperienceCard.vue
+++ components/ExperienceCard.vue
@@ -5,12 +5,12 @@
       class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-blue-500/10 dark:lg:group-hover:bg-slate-800/50 dark:lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] dark:lg:group-hover:drop-shadow-lg">
     </div>
     <header class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
-      aria-label="`${date}`">{{ date }}</header>
+      :aria-label="date">{{ date }}</header>
     <div class="z-10 sm:col-span-6">
       <h3 class="font-medium leading-snug text-slate-200">
         <div><a
             class="group/link inline-flex items-baseline text-lg font-medium leading-tight text-violet-900 focus-visible:text-teal-300 dark:text-slate-200 dark:hover:text-teal-300"
-            :href="link" target="_blank" rel="noreferrer" aria-label="`${job} at ${company}`"><span
+            :href="link" target="_blank" rel="noreferrer" :aria-label="`${job} at ${company}`"><span
               class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span><span>
               {{ job }} at <span class="inline-block whitespace-nowrap"> {{ company }}
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
