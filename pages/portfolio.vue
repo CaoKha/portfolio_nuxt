0514db5f6eb88d3d6e1ff68ede400fa587736399
Kha Nguyen <nguyencaokha131995@gmail.com>
2024-02-20 11:11:13 +0100

    [refactor] move constants to composable

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -110,64 +110,7 @@
 <script lang="ts" setup>
 import { vIntersectionObserver } from '@vueuse/components'
 
-const headers = [
-  'About',
-  'Experiences',
-  'Projects'
-]
-
-const experiences = [
-  {
-    date: 'Sep 2023 — Present',
-    job: 'Embedded Software Engineer',
-    company_name: 'Poclain Hydraulics',
-    link: 'https://poclain-hydraulics.com/',
-    content: 'Automating data analysis processes of many legacy C/C++ projects. \
-    Creating automation Rust scripts and CLI tool, transforming legacy PLM documents into git history for better tracking system',
-    tech_stack: ['Rust', 'Python', 'C', 'C++', 'Batch File', 'Polars', 'Makefile', 'Cmake']
-  },
-  {
-    date: 'Sep 2021 — Sep 2023',
-    job: 'Fullstack Engineer',
-    company_name: 'Federation Francaise de Football',
-    link: 'https://www.fff.fr/',
-    content: 'Developed and maintained multiple applications and tools for internal FFF employees \
-              such as payment app, event organizers app, document management app, etc',
-    tech_stack: ['React', 'Angular', 'HTML', 'CSS', 'Python', 'FastAPI', 'Javascript', 'Typescript',
-      'PrimeNG', 'MaterialUI', 'Bootstrap', 'TailwindCSS', 'Traefik', 'Azure', 'MongoDB', 'Redis']
-  },
-  {
-    date: 'Sep 2020 — Mars 2021',
-    job: 'Deep Learning Engineer',
-    company_name: 'Fintricity',
-    link: 'https://www.fintricity.com/',
-    content: 'Researched and developed a Deep Learning Model for Weed Detection App. \
-              The Deep Learning Model can differentiate different growth stage of the plant \
-              Worked with a researcher to implement his solution into Amazon Web Service',
-    tech_stack: ['Python', 'Numpy', 'FastAI', 'OpenCV', 'AWS']
-  },
-  {
-    date: 'Mars 2019 — Sep 2019',
-    job: 'Robotics Engineer',
-    company_name: 'Trimble Inc',
-    link: 'https://www.trimble.com/',
-    content: 'Researched and developed a synchronization method between motion sensors and camera on a mobile robot. \
-              Worked with a researcher to implement his solution onto navigation system of the company',
-    tech_stack: ['C++', 'CMake', 'Boost', 'OpenCV', 'Python', 'FPGA']
-  }
-];
-
-const projects = [
-  {
-    title: 'Finite State Machine Mobile Robot',
-    link: '/projects',
-    description: 'A robot that can push any object out of a using a luminosity sensor and infrared sensor. ring \
-                  Implementation of finite state machine methodology',
-    imgSrc: '/images/sumo_robot.png',
-    tech_stack: ['C', 'Arduino', 'Blender']
-  },
-];
-
+const {headers, experiences, projects} = useConstants();
 const portfolio_body = useState('portfolio_body', () => null)
 const currentSection = useState('currentSection', () => null as string | null)
 
---
+++ pages/projects/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="min-h-screen py-10">
+    <section id="Projects" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Selected projects">
+      <div
+        class="sticky top-0 z-20 mb-4 w-screen bg-blue-500/10 px-6 py-5 backdrop-blur dark:bg-slate-900/75 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+        <h2 class="text-sm font-bold uppercase tracking-widest text-slate-700 dark:text-slate-200 lg:sr-only">Projects
+        </h2>
+      </div>
+      <div>
+        <ul>
+          <li class="mb-12 px-2" v-for="project in projects">
+            <ProjectCard :title="project.title" :link="project.link" :description="project.description"
+              :imgSrc="project.imgSrc" :tech_stack="project.tech_stack" />
+          </li>
+        </ul>
+      </div>
+    </section>
+  </main>
+</template>
+  
+<script setup lang="ts">
+const { projects } = useConstants();
+</script>
+  
+<style scoped></style>
