e8f64612b349d87230d5027f8f78448b62b4fae5
Kha <nguyencaokha131995@gmail.com>
2024-04-09 15:56:30 +0200

    add message when open in mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
-  modules: ["@element-plus/nuxt", "@nuxt/image", "@artmizu/nuxt-prometheus", "@pinia/nuxt", "@vueuse/nuxt"],
+  modules: ["@element-plus/nuxt", "@nuxt/image", "@artmizu/nuxt-prometheus", "@pinia/nuxt", "@vueuse/nuxt", "@nuxtjs/device"],
   postcss: {
     plugins: {
       tailwindcss: {},
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <main class="relative flex min-h-screen flex-col items-center justify-start p-4">
-    <h2 class="text-xl text-slate-500 font-semibold dark:text-slate-200 m-4">-- My Yogurt --</h2>
-
+    <h2 class="text-xl text-slate-500 font-semibold dark:text-slate-200 m-4">-- Gizmo Testing --</h2>
     <div class="w-full h-full flex items-center justify-center">
-      <iframe class="w-[1280px] h-[720px]" id="bevy-game" src="/game/index.html"></iframe>
+      <iframe v-if="isDesktop" class="w-[1280px] h-[720px]" id="bevy-game" src="/game/index.html"
+        allow="fullscreen"></iframe>
+      <h3 v-if="!isDesktop" class="text-xl text-slate-500 font-semibold dark:text-slate-200 m-4"> Open in desktop to see
+        the gizmo</h3>
     </div>
   </main>
 </template>
@@ -12,4 +14,5 @@
 import { useUsersStore } from '@/stores/users'
 const usersStore = useUsersStore()
 await usersStore.fetchUsers()
+const { isDesktop } = useDevice()
 </script>
